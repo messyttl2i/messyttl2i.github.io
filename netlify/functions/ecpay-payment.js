0c550077c96ae5958765af1db1998757248b3ea1
messyttl2i <messytt200002@gmail.com>
2026-04-22 09:47:36 +0800

    Update ecpay-payment.js

--- netlify/functions/ecpay-payment.js
+++ netlify/functions/ecpay-payment.js
@@ -1,1 +1,15 @@
-const NETLIFY_SITE_URL = (process.env.SITE_URL || process.env.NETLIFY_SITE_URL || '').replace(/\/$/, '');
+// api/ecpay-payment.js
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
+
+  if (req.method === 'OPTIONS') return res.status(204).send('');
+  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+
+  return res.status(501).json({
+    success: false,
+    error: 'EcpayPaymentNotImplemented',
+    message: 'ecpay-payment 尚未完成，請先完成物流選店與建物流單。'
+  });
+};
